9795b6fb89dd4770d40e99a274a6381d745a2d71
yamakasy267 <46868991+yamakasy267@users.noreply.github.com>
2021-01-27 22:42:08 +0300

    Add files via upload

---
+++ kurs_shop_new/kurs_shop/kurs_shop/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace kurs_shop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.delete = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.quastion = new System.Windows.Forms.Button();
+            this.finish = new System.Windows.Forms.Label();
+            this.basket = new System.Windows.Forms.DataGridView();
+            this.print = new System.Windows.Forms.Button();
+            this.paper = new System.Windows.Forms.Label();
+            this.quantity = new System.Windows.Forms.TextBox();
+            this.check = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.products = new System.Windows.Forms.ListBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.basket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(6);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(751, 452);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage1.BackgroundImage")));
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage1.Controls.Add(this.delete);
+            this.tabPage1.Controls.Add(this.add);
+            this.tabPage1.Controls.Add(this.save);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 33);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(6);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(6);
+            this.tabPage1.Size = new System.Drawing.Size(743, 415);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Data Base";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // delete
+            // 
+            this.delete.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("delete.BackgroundImage")));
+            this.delete.Location = new System.Drawing.Point(582, 356);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(103, 38);
+            this.delete.TabIndex = 4;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // add
+            // 
+            this.add.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("add.BackgroundImage")));
+            this.add.Location = new System.Drawing.Point(308, 356);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(106, 38);
+            this.add.TabIndex = 3;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // save
+            // 
+            this.save.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("save.BackgroundImage")));
+            this.save.Location = new System.Drawing.Point(62, 356);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(104, 38);
+            this.save.TabIndex = 2;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(744, 338);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
+            this.dataGridView1.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridView1_DataError);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage2.BackgroundImage")));
+            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage2.Controls.Add(this.quastion);
+            this.tabPage2.Controls.Add(this.finish);
+            this.tabPage2.Controls.Add(this.basket);
+            this.tabPage2.Controls.Add(this.print);
+            this.tabPage2.Controls.Add(this.paper);
+            this.tabPage2.Controls.Add(this.quantity);
+            this.tabPage2.Controls.Add(this.check);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.products);
+            this.tabPage2.Location = new System.Drawing.Point(4, 33);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(6);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(6);
+            this.tabPage2.Size = new System.Drawing.Size(743, 415);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Cashier";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // quastion
+            // 
+            this.quastion.Location = new System.Drawing.Point(700, 13);
+            this.quastion.Name = "quastion";
+            this.quastion.Size = new System.Drawing.Size(34, 34);
+            this.quastion.TabIndex = 8;
+            this.quastion.Text = "?";
+            this.quastion.UseVisualStyleBackColor = true;
+            this.quastion.Click += new System.EventHandler(this.quastion_Click);
+            // 
+            // finish
+            // 
+            this.finish.AutoSize = true;
+            this.finish.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.finish.ForeColor = System.Drawing.Color.White;
+            this.finish.Location = new System.Drawing.Point(307, 252);
+            this.finish.Name = "finish";
+            this.finish.Size = new System.Drawing.Size(0, 25);
+            this.finish.TabIndex = 7;
+            // 
+            // basket
+            // 
+            this.basket.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.basket.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.basket.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.basket.CausesValidation = false;
+            this.basket.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.basket.Location = new System.Drawing.Point(307, 49);
+            this.basket.Name = "basket";
+            this.basket.Size = new System.Drawing.Size(357, 196);
+            this.basket.TabIndex = 6;
+            
+            // 
+            // print
+            // 
+            this.print.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("print.BackgroundImage")));
+            this.print.Enabled = false;
+            this.print.Location = new System.Drawing.Point(420, 339);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(137, 36);
+            this.print.TabIndex = 5;
+            this.print.Text = "Print check";
+            this.print.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.print.UseVisualStyleBackColor = true;
+            this.print.Click += new System.EventHandler(this.print_Click);
+            // 
+            // paper
+            // 
+            this.paper.AutoSize = true;
+            this.paper.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.paper.ForeColor = System.Drawing.Color.White;
+            this.paper.Location = new System.Drawing.Point(309, 22);
+            this.paper.Name = "paper";
+            this.paper.Size = new System.Drawing.Size(0, 25);
+            this.paper.TabIndex = 4;
+            // 
+            // quantity
+            // 
+            this.quantity.Location = new System.Drawing.Point(15, 262);
+            this.quantity.Name = "quantity";
+            this.quantity.Size = new System.Drawing.Size(245, 33);
+            this.quantity.TabIndex = 3;
+            this.quantity.TextChanged += new System.EventHandler(this.quantity_TextChanged);
+            this.quantity.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.quantity_KeyPress);
+            // 
+            // check
+            // 
+            this.check.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("check.BackgroundImage")));
+            this.check.Enabled = false;
+            this.check.Location = new System.Drawing.Point(68, 338);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(147, 37);
+            this.check.TabIndex = 2;
+            this.check.Text = "Add";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(52, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose product";
+            // 
+            // products
+            // 
+            this.products.FormattingEnabled = true;
+            this.products.ItemHeight = 24;
+            this.products.Location = new System.Drawing.Point(15, 49);
+            this.products.Name = "products";
+            this.products.Size = new System.Drawing.Size(245, 196);
+            this.products.TabIndex = 0;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 462);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Vegetable Shop";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.basket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox products;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.TextBox quantity;
+        private System.Windows.Forms.Label paper;
+        private System.Windows.Forms.Button print;
+        private System.Windows.Forms.DataGridView basket;
+        private System.Windows.Forms.Label finish;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Button quastion;
+    }
+}
+
---
+++ kurs_shop_new/kurs_shop/kurs_shop/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Drawing.Printing;
+using System.IO;
+
+namespace kurs_shop
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='|DataDirectory|\Database1.mdf';Integrated Security=True";
+        private string sql = "SELECT * FROM Products";
+        private SqlDataAdapter adapter { get; set; }
+        private DataSet dataSet { get; set; }
+        private SqlCommandBuilder commandBuilder { get; set; }
+        private List<string> listOfProducts { get; set; }
+        private DataTable purchases { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            purchases = new DataTable();
+            listOfProducts = new List<string>();
+            dataSet = new DataSet("productsSet");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    adapter = new SqlDataAdapter(sql, connection);
+                    dataSet.Tables.Add(new DataTable("products"));
+                    adapter.Fill(dataSet.Tables["products"]);
+                    dataGridView1.DataSource = dataSet.Tables["products"];
+                }
+                for(int i=0; i<dataSet.Tables["products"].Rows.Count; i++)
+                    listOfProducts.Add(dataSet.Tables["products"].Rows[i]["name"].ToString());
+                products.DataSource = listOfProducts;
+                purchases.Columns.Add("Name", typeof(String));
+                purchases.Columns.Add("Quantity", typeof(String));
+                purchases.Columns.Add("PPO", typeof(String));
+                purchases.Columns.Add("Amount", typeof(String));
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("No Connection." + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    adapter = new SqlDataAdapter(sql, connection);
+                    commandBuilder = new SqlCommandBuilder(adapter);
+                    adapter.Update(dataSet,"products");
+                    MessageBox.Show("Table saved!", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Data base didn't saved! Error code: " + ex.Message, "Notice", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            DataRow row = dataSet.Tables[0].NewRow();
+            dataSet.Tables["products"].Rows.Add(row);
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                    dataGridView1.Rows.Remove(row);
+                save_Click(null, null);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Wrong action! Code error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            paper.Text = "Cash register receipt";
+            DataRow TRow = purchases.NewRow();
+            float quanty = Convert.ToSingle(quantity.Text.ToString());
+            foreach (DataRow row in dataSet.Tables["products"].Rows)
+                if (row["name"].ToString() == products.SelectedItem.ToString())
+                {
+                    TRow["Name"] = row["name"].ToString();
+                    TRow["Quantity"] = quanty.ToString();
+                    TRow["PPO"] = row["price"].ToString();
+                    TRow["Amount"] = (quanty * Convert.ToSingle(row["price"])).ToString();
+                }
+            purchases.Rows.Add(TRow);
+            basket.DataSource = purchases;
+            finish.Text= "Date..........................." + DateTime.Now.ToShortDateString() + "\n" +
+                         "Time..........................." + DateTime.Now.ToLongTimeString() + "\n" +
+                         "Thank you for your purchase!";
+            print.Enabled = paper.Text != string.Empty;
+        }
+
+        private void quantity_TextChanged(object sender, EventArgs e)
+        {
+            if (quantity.Text != string.Empty)
+                check.Enabled = true;
+        }
+
+        private void quantity_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ',' && new string((quantity.Text + e.KeyChar).ToCharArray())[0] == ',')
+            {
+                e.Handled = true;
+                return;
+            }
+            if (Char.IsControl(e.KeyChar))
+                return;
+            if (e.KeyChar == '.')
+                e.KeyChar = ',';
+            if (e.KeyChar.ToString() == string.Empty || int.TryParse(e.KeyChar.ToString(), out _) || e.KeyChar == ',')
+                return;
+            e.Handled = true;
+        }
+        
+        private void print_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+            for(int i=0; i < purchases.Rows.Count; i++)
+                for (int j = 0; j < dataSet.Tables[0].Rows.Count; j++)
+                    if (purchases.Rows[i]["Name"].Equals(dataSet.Tables[0].Rows[j]["Name"]))
+                        dataSet.Tables[0].Rows[j]["quantity"] = Convert.ToSingle(dataSet.Tables[0].Rows[j]["quantity"]) - Convert.ToSingle(purchases.Rows[i]["quantity"]);
+            save_Click(null, null);
+            purchases.Clear();
+            MessageBox.Show("Check printed successfully!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            DataGridView b = basket;
+            b.RowHeadersVisible = false;
+            b.Size = new Size(430,204);
+            Bitmap bmp = new Bitmap(b.Size.Width, b.Size.Height);
+            b.DrawToBitmap(bmp, new Rectangle(0,0, b.Size.Width, b.Size.Height));
+            e.Graphics.DrawString(paper.Text.ToString(), new Font("Arial", 14), Brushes.Black, 0, 15);
+            e.Graphics.DrawImage(bmp, 0, 50);
+            e.Graphics.DrawString(finish.Text.ToString(), new Font("Arial", 14), Brushes.Black, 0, b.Size.Height+70);
+        }
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            if (e.FormattedValue.ToString() == string.Empty)
+                return;
+            switch (e.ColumnIndex)
+            {
+                case 0:
+                    if (!int.TryParse(e.FormattedValue.ToString(), out _))
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+                    break;
+                case 3:
+                case 4:
+                    if (!float.TryParse(e.FormattedValue.ToString(), out _))
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+                    break;
+                case 5:
+                    if (!DateTime.TryParse(e.FormattedValue.ToString(), out _))
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+                    break;
+            }
+        }
+
+        private void dataGridView1_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            if(e.Exception != null && e.Context == DataGridViewDataErrorContexts.Commit)
+                MessageBox.Show("Incorrect data! Please reenter! Error code:"+e.Exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        
+        private void quastion_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The application <Vegetable Shop> is a database of products in the store.\n Here you can add new types of products and also change their price.\n To add a new product, click on the <Add> button. To delete <Delete>.\n You can also collect your own shopping cart and print a receipt.\n The work was performed by Ilya Shevtsov and Artem Shershakov.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
